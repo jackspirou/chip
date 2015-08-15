b250068febaeba909d33e5056c1bd57cebcd9999
jackspirou <jack.spirou@target.com>
2015-08-15 08:57:28 -0500

    making imports optional

--- parser/nextImports.go
+++ parser/nextImports.go
@@ -6,12 +6,27 @@ import "github.com/jackspirou/chip/token"
 func (p *Parser) nextImports() {
 	p.enter()
 
-	p.nextExpected(token.IMPORT)
+	if p.tok.Type == token.IMPORT {
+		p.next()
 
-	if p.tok.Type == token.LPAREN {
-		p.next() // skip '('
+		if p.tok.Type == token.LPAREN {
+			p.next() // skip '('
 
-		for p.tok.Type != token.RPAREN {
+			for p.tok.Type != token.RPAREN {
+				if p.tok.Type == token.IDENT {
+					// p.tok.String() // get package alias
+					p.next()
+					// p.tok.String() // get package src
+					p.nextExpected(token.STRING)
+				} else {
+					// p.tok.String() // get package src
+					p.nextExpected(token.STRING)
+				}
+			}
+
+			p.nextExpected(token.RPAREN)
+
+		} else {
 			if p.tok.Type == token.IDENT {
 				// p.tok.String() // get package alias
 				p.next()
@@ -23,18 +38,6 @@ func (p *Parser) nextImports() {
 			}
 		}
 
-		p.nextExpected(token.RPAREN)
-
-	} else {
-		if p.tok.Type == token.IDENT {
-			// p.tok.String() // get package alias
-			p.next()
-			// p.tok.String() // get package src
-			p.nextExpected(token.STRING)
-		} else {
-			// p.tok.String() // get package src
-			p.nextExpected(token.STRING)
-		}
 	}
 	p.exit()
 }
